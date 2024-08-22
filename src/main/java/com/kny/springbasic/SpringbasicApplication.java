bfe68785f7eb7ed180bd4c0bd838514f0ebd94de
nanayeon <tires0301@gmail.com>
2024-08-22 15:51:49 +0900

    3:50

--- src/main/java/com/kny/springbasic/SpringbasicApplication.java
+++ src/main/java/com/kny/springbasic/SpringbasicApplication.java
@@ -3,11 +3,27 @@ package com.kny.springbasic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.kny.springbasic.dto.Lombok;
+
 @SpringBootApplication
 public class SpringbasicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbasicApplication.class, args);
+	}
+
+	void lombokExample() {
+
+		Lombok lombok = new Lombok("a", "b", "c", false, false);
+		lombok.getField1();
+		lombok.setField3(null);
+
+		//lombok = new Lombok();
+		lombok = new Lombok("d", "e");
+
+		lombok.isField4();
+		lombok.getField5();
+
 	}
 
 }
